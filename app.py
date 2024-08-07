43b931885401228540b5b68a94f7d5e9627d038f
Suyog Sanjay Igave <103380697+suyogigave@users.noreply.github.com>
2024-08-07 11:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def cleanup_upload_folder():
         print(f'Error cleaning up upload folder: {str(e)}')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
